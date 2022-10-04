93b07879c160c53b3cfd4762ac992f1f32ef98e0
rodrigovk <rodrigocasadeialanis@hotmail.com>
2022-10-04 18:03:42 -0300

    Melhorias e correções

--- assets/vee-validate.esm.84af39ca.js
+++ assets/vee-validate.esm.84af39ca.js
@@ -1,4 +1,4 @@
-import{d as Se,O as ee,P as Be,h as be,Q as Ye,l as z,y as f,o as Xe,R as Qe,S as Ae,w as X,a as Ze,r as Q,U as Re,V as ye,W as Je,X as gn,Y as xe,Z as Vn}from"./index.565e4036.js";/**
+import{d as Se,O as ee,P as Be,h as be,Q as Ye,l as z,y as f,o as Xe,R as Qe,S as Ae,w as X,a as Ze,r as Q,U as Re,V as ye,W as Je,X as gn,Y as xe,Z as Vn}from"./index.d52f0d7c.js";/**
   * vee-validate v4.6.5
   * (c) 2022 Abdelrahman Awad
   * @license MIT
